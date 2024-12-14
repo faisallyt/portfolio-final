dc1e2fedf66b15919d3637914a19dfe674c61b74
faisallyt <fdiledu@gmail.com>
2024-12-15 04:29:14 +0530

    fix

--- app/api/sentry-example-api/route.js
+++
@@ -1,8 +0,0 @@
-// app/api/sentry-example-api/route.js
-
-import { NextResponse } from "next/server";
-
-export function GET() {
-  throw new Error("Sentry Example API Route Error");
-  return NextResponse.json({ data: "Testing Sentry Error..." });
-}
